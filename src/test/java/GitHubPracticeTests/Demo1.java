2be23343a45b26d2ba03567d61448636f49f896f
Sumeet Juvatkar <Sumeet Juvatkar@SUMO-pc>
2026-02-14 12:17:09 +0530

    first time commiting

---
+++ src/test/java/GitHubPracticeTests/Demo1.java
@@ -0,0 +1,13 @@
+package GitHubPracticeTests;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+	@Test(groups = "smoke")
+	public void cars()
+	{
+		System.out.println("Benz");
+	}
+
+}
---
+++ src/test/java/GitHubPracticeTests/Demo2.java
@@ -0,0 +1,13 @@
+package GitHubPracticeTests;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+	
+	@Test
+	public void plans()
+	{
+		System.out.println("Indigo");
+	}
+
+}
